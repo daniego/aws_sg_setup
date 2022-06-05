d06130c62cbfa2ae7defc02270500b1d2b006804
Daniel Floris <daniel.floris@gmail.com>
2022-06-05 08:59:12 +0100

    Added table with instances and tasks description

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.8
-RUN pip install boto3 requests
+RUN pip install boto3 requests tabulate
 ADD update_sg.py /
 ENTRYPOINT ["python", "/update_sg.py"]
--- update_sg.py
+++ update_sg.py
@@ -4,12 +4,15 @@ from requests import get
 from getpass import getpass
 from os import environ
 import json
-
+from pprint import pprint
+from tabulate import tabulate
+import re
 
 security_group_id     = environ.get('AWS_SG')
-region_name           = environ.get('AWS_REGION')
+region_name           = environ.get('AWS_REGION', 'eu-west-1')
 aws_access_key_id     = environ.get('AWS_ACCESS_KEY')
 aws_secret_access_key = environ.get('AWS_SECRET_KEY')
+ecs_cluster           = environ.get('ECS_CLUSTER', 'nsp-staging')
 ports=json.loads(environ.get('ports', []))
 
 aws_secret_access_key = getpass('AWS_SECRET_KEY:')
@@ -51,3 +54,76 @@ for port in ports:
     },
     ])
     print("Added port {}/{} from IP {}".format(port['port'], port['protocol'], ip))
+
+
+
+
+print("#\n### Instances\n#")
+
+ec2_workers  = ec2_client.describe_instances(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': [
+                'nsp-staging-ecs_worker',
+            ]
+        },
+    ],
+)
+
+instance_table = [
+    ['instance_id', 'Public IP']
+]
+
+for ec2_worker in ec2_workers['Reservations']:
+    instance_id = ec2_worker['Instances'][0]['InstanceId']
+    public_ip = ec2_worker['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicIp']
+    instance_table.append([instance_id, public_ip])
+
+print(tabulate(instance_table, headers='firstrow', tablefmt='grid', showindex=True))
+
+
+
+print("#\n### Containers\n#")
+
+ecs_client=boto3.client(
+    'ecs',
+    aws_access_key_id = aws_access_key_id,
+    aws_secret_access_key = aws_secret_access_key,
+    region_name = region_name
+)
+
+tasks = ecs_client.list_tasks(
+    cluster=ecs_cluster
+)
+
+tasks_specs = ecs_client.describe_tasks(
+    cluster=ecs_cluster,
+    tasks=tasks['taskArns'],
+    include=[
+        'TAGS',
+    ]
+)
+
+task_table = [
+    ['Task', 'Status', 'Started At', 'Instance', 'CPU', 'Memory'],
+]
+
+for task in tasks_specs['tasks']:
+
+    # Container instance
+    container_instance = ecs_client.describe_container_instances(
+        cluster=ecs_cluster,
+        containerInstances=[task['containerInstanceArn']],
+    )
+
+    task_table.append([
+        re.sub('.*\/', '', task['taskDefinitionArn']),
+        task['containers'][0]['lastStatus'],
+        task['startedAt'],
+        container_instance['containerInstances'][0]['ec2InstanceId'],
+        task['cpu'],
+        task['memory'],
+    ])
+
+print(tabulate(task_table, headers='firstrow', tablefmt='grid', showindex=True))
